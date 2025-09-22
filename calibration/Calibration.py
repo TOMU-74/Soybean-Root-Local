d1a9cc59091ea8e15a4e283a77d02eeedb7a402b
TOMOKI SUDO <136161374+TOMU-74@users.noreply.github.com>
2025-09-22 17:53:40 +0900

    change:キャイリブレーションコードの完成

--- calibration/Calibration.py
+++ calibration/Calibration.py
@@ -28,10 +28,6 @@ for fname in train_images:
     img = cv2.imread(fname)
     #グレースケール
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    #gray = cv2.resize(gray_b, (2080,2080))
-    #ret1, gray = cv2.threshold(img, 100,255, cv2.THRESH_BINARY)
-    #cv2.imshow('img',gray)
-    #cv2.waitKey(0)
     
     #チェスボードの角を検出
     ret, corners = cv2.findChessboardCorners(gray, (7,10), None)
@@ -43,18 +39,21 @@ for fname in train_images:
         corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
         #2Dの点を追加
         imgpoints.append(corners2)
-
         # 検出結果を描画
         img = cv2.drawChessboardCorners(img, (7,10), corners2,ret)
         #cv2.imshow('img',img)
         #cv2.waitKey(0)
-        #画像を保存
+        # 画像を保存
         cv2.imwrite("cal{0}.jpg".format(i),img)
         i = i+1
         print("Yes point: {}".format(fname))
     else:
         print("No point: {}".format(fname))
 cv2.destroyAllWindows()
+
+#===========キャリブレーションの実行================
+#キャリブレーションの実行
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
 
 #再投影誤差の検証
 mean_error = 0
@@ -65,66 +64,6 @@ for i in range(len(objpoints)):
 #0.0405874093573348
 
 print("total error: ", mean_error/len(objpoints))
-
-
-'''
-#===========キャリブレーションの実行================
-#キャリブレーションの実行
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-
-print("objpoints:{}".format(objpoints))
-print("imgpoints:{}".format(imgpoints))
-print("gray.shape[::-1]:{}".format(gray.shape[::-1]))
-print("ret:{}".format(ret))
-print("mtx:{}".format(mtx))
-print("dist:{}".format(dist))
-print("rvecs:{}".format(rvecs))
-print("tvecs:{}".format(tvecs))
-
-#キャリブレーション結果の保存
-#np.save('np_save', objpoints=objpoints, imgpoints=imgpoints, grayshape=gray.shape[::-1],ret=ret, mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
-np.savez('np_save1', objpoints, imgpoints, gray.shape[::-1])
-np.savez('np_save2', ret, mtx, dist)
-np.savez('np_save3', rvecs, tvecs)
-
-
-
-
-#===========キャリブレーション結果の読み込み================
-#保管したデータを取り出す。
-np_save1 = np.load('np_save1.npz')
-np_save2 = np.load('np_save2.npz')
-np_save3 = np.load('np_save3.npz')
-
-"""
-#print(np_save1.files)
-#print(np_save2.files)
-#print(np_save3.files)
-print(np_save1['arr_0'])
-print(np_save1['arr_1'])
-print(np_save1['arr_2'])
-print(np_save2['arr_0'])
-print(np_save2['arr_1'])
-print(np_save2['arr_2'])
-print(np_save3['arr_0'])
-print(np_save3['arr_1'])
-"""
-#それぞれのデータを変数に格納
-#3Dの点
-objpoints = np_save1['arr_0']
-#2Dの点
-imgpoints = np_save1['arr_1']
-#mtx: カメラ行列、dist: 歪み係数, rvecs: 回転ベクトル, tvecs: 平行移動ベクトル
-ret, mtx, dist, rvecs, tvecs = np_save2['arr_0'], np_save2['arr_1'], np_save2['arr_2'], np_save3['arr_0'], np_save3['arr_1']
-
-
-"""
-print("ret:{}".format(ret))
-print("mtx:{}".format(mtx))
-print("dist:{}".format(dist))
-print("rvecs:{}".format(rvecs))
-print("tvecs:{}".format(tvecs))
-"""
 
 #===========歪み補正================
 #補正画像の読み込み
@@ -145,9 +84,3 @@ for fname in images2:
     dst = dst[y:y+h, x:x+w]
     #修正後の画像を保存
     cv2.imwrite('./output/' + ftitle + '_cal' + fext,dst)
-
-
-
-
-'''
-#1,2,5,7,9
